a1136647c7bb2c41c23fab58c5e09cdc12929187
Renegod <jakiemakar@gmail.com>
2017-03-01 22:59:43 -0500

    Update drivetrain.py

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -5,5 +5,9 @@ from commands.followjoystick import FollowJoystick
 import robotmap
 
 class drivetrain:
-  
-
+  def __init__(self)
+     super().__init__()
+    self.motor0 = wpilib.Spark(robotMap.DRIVE0_PORT)
+    self.motor1 = wpilib.Spark(robotMap.DRIVE1_PORT)
+    self.motor2 = wpilib.Spark(robotMap.DRIVE2_PORT)
+    self.motor3 = wpilib.Spark(robotMap.DRIVE3_PORT)
