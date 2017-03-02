b1fcd8ce58b441c654c06e68894d5a8f0722fb83
Renegod <jakiemakar@gmail.com>
2017-03-01 20:00:14 -0500

    Create robotmap.py

---
+++ robotmap.py
@@ -0,0 +1,14 @@
+class PortsList:
+    # left drive ports
+    DRIVE0_PORT = 0
+    DRIVE1_PORT = 1
+    # right drive ports
+    DRIVE2_PORT = 2
+    DRIVE3_PORT = 3
+    # controller ports
+    LOGITECH_PORT = 0
+    JOYSTICK_FB_PORT = 1
+    JOYSTICK_LR_PORT = 2
+    JOYSTICK_SENS_PORT = 3
+    JOYSTICK_FASTTURN_BUTTON = 2
+    JOYSTICK_NOTURN_BUTTON = 1
